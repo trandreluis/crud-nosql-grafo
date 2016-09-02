416af0c866a08b154a10dd35620f5e3c3feb6b20
Andre Luis <tr.andreluis@gmail.com>
2016-09-01 22:20:28 -0300

    Criacao da primeira tela

---
+++ crud-neo4j/src/main/java/br/edu/ifpb/monteiro/ads/view/OuvinteInicio.java
@@ -0,0 +1,24 @@
+package br.edu.ifpb.monteiro.ads.view;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class OuvinteInicio implements ActionListener {
+
+	private Inicio janelaInicio;
+	
+	public OuvinteInicio(Inicio janelaInicio) {
+		this.janelaInicio = janelaInicio;
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource() ==)
+		
+	}
+
+	public void botaoCadastrar() {
+		
+	}
+	
+}
