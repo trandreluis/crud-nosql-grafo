b484cb336e6f8c4c2772cb3a4962f516bef48bdc
Andre Luis <tr.andreluis@gmail.com>
2016-09-01 22:21:58 -0300

    Aplicacao de Look and Feel

--- crud-neo4j/src/main/java/br/edu/ifpb/monteiro/ads/view/Main.java
+++ crud-neo4j/src/main/java/br/edu/ifpb/monteiro/ads/view/Main.java
@@ -11,7 +11,35 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		
+		try {
+			for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager
+					.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					javax.swing.UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (ClassNotFoundException ex) {
+			java.util.logging.Logger.getLogger(Main.class.getName()).log(
+					java.util.logging.Level.SEVERE, null, ex);
+		} catch (InstantiationException ex) {
+			java.util.logging.Logger.getLogger(Main.class.getName()).log(
+					java.util.logging.Level.SEVERE, null, ex);
+		} catch (IllegalAccessException ex) {
+			java.util.logging.Logger.getLogger(Main.class.getName()).log(
+					java.util.logging.Level.SEVERE, null, ex);
+		} catch (javax.swing.UnsupportedLookAndFeelException ex) {
+			java.util.logging.Logger.getLogger(Main.class.getName()).log(
+					java.util.logging.Level.SEVERE, null, ex);
+		}
+
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				Inicio a = new Inicio();
+				a.setLocationRelativeTo(null);
+				a.setVisible(true);
+			}
+		});
 		
 	}
 	
