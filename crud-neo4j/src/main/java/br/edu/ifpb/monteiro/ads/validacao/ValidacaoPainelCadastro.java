29b70018524b1e4b0fcd101714134b9db792de07
Andre Luis <tr.andreluis@gmail.com>
2016-09-03 21:00:29 -0300

    Alteracao na instanciacao do validador

--- crud-neo4j/src/main/java/br/edu/ifpb/monteiro/ads/validacao/ValidacaoPainelCadastro.java
+++ crud-neo4j/src/main/java/br/edu/ifpb/monteiro/ads/validacao/ValidacaoPainelCadastro.java
@@ -1,6 +1,5 @@
 package br.edu.ifpb.monteiro.ads.validacao;
 
-import br.edu.ifpb.monteiro.ads.excecoes.NomeInvalidoException;
 import br.edu.ifpb.monteiro.ads.view.OuvintePainelCadastro;
 import br.edu.ifpb.monteiro.ads.view.PainelCadastro;
 
@@ -8,22 +7,29 @@ public class ValidacaoPainelCadastro {
 
 	private OuvintePainelCadastro ouvinte;
 	private PainelCadastro painel;
-	
+
 	public ValidacaoPainelCadastro(OuvintePainelCadastro ouvintePainel) {
 		this.ouvinte = ouvintePainel;
 		this.painel = ouvintePainel.getPainel();
 	}
-	
-	public void validacaoCadastrar(String nome, String sobrenome, String cpf, String idade) throws NomeInvalidoException {
-		
-		if(nome.equals("") || nome == null) {
-			throw new NomeInvalidoException();
+
+	public boolean validacaoCadastrar() {
+
+		String nome = painel.getFieldNome().getText();
+		String sobrenome = painel.getFieldSobrenome().getText();
+		String idade = painel.getFieldIdade().getText();
+		String cpf = painel.getFieldCPF().getText();
+
+		if (nome == null || sobrenome == null || idade == null || cpf == null) {
+			return false;
 		}
-		
+
+		if (nome.equals("") || sobrenome.equals("") || idade.equals("") || cpf.equals("   .   .   -  ")) {
+			return false;
+		}
+
+		return true;
+
 	}
-	
-	public void validacaoCancelar() {
-		
-	}
-	
+
 }
